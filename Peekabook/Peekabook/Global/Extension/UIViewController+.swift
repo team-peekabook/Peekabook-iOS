1999b7599b4da339bf8d55f5087ae1e79096923b
devxsby <devxsby@gmail.com>
2022-12-31 23:55:34 +0900

    [Chore] #1- UIViewController+ 수정

--- Peekabook/Peekabook/Global/Extension/UIViewController+.swift
+++ Peekabook/Peekabook/Global/Extension/UIViewController+.swift
@@ -13,15 +13,21 @@ extension UIViewController {
      - Description: 화면 터치시 작성 종료
      */
     /// 화면 터치시 작성 종료하는 메서드
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    open override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
     }
-    
+
     /**
      - Description: 화면 터치시 키보드 내리는 Extension
      */
-    @objc
-    func dismissKeyboard() {
+    /// 화면 터치시 키보드 내리는 메서드
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+
+    @objc func dismissKeyboard() {
         view.endEditing(true)
     }
 }
