598cd7f2c5856a059265878be4c70a473282e9c8
devxsby <devxsby@gmail.com>
2022-12-31 23:49:56 +0900

    [Feat] #1- UIView+ Extension 추가

---
+++ Peekabook/Peekabook/Global/Extension/UIView+.swift
@@ -0,0 +1,27 @@
+//
+//  UIView+.swift
+//  Peekabook
+//
+//  Created by devxsby on 2022/12/31.
+//
+
+import UIKit
+
+extension UIView {
+    
+    func setGradient() {
+        let gradient = CAGradientLayer()
+        gradient.frame = bounds
+        gradient.colors = [UIColor.init(white: 1, alpha: 0).cgColor, UIColor.init(white: 1, alpha: 1).cgColor]
+        gradient.locations = [0.0, 0.8, 1.0]
+        gradient.startPoint = CGPoint(x: 1.0, y: 0.2)
+        gradient.endPoint = CGPoint(x: 1.0, y: 1)
+        layer.insertSublayer(gradient, at: 0)
+    }
+    
+    func roundCorners(cornerRadius: CGFloat, maskedCorners: CACornerMask) {
+        clipsToBounds = true
+        layer.cornerRadius = cornerRadius
+        layer.maskedCorners = CACornerMask(arrayLiteral: maskedCorners)
+    }
+}
