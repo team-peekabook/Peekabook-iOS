1aaac39ce8dd785343a92a1f9b4a684b64f9fbff
devxsby <devxsby@gmail.com>
2022-12-31 23:47:53 +0900

    [Feat] #1- UIViewController+ Extension 추가

---
+++ Peekabook/Peekabook/Global/Extension/UIViewController+.swift
@@ -0,0 +1,27 @@
+//
+//  UIViewController+.swift
+//  Peekabook
+//
+//  Created by devxsby on 2022/12/31.
+//
+
+import UIKit
+
+public extension UIViewController {
+    
+    /**
+     - Description: 화면 터치시 작성 종료
+     */
+    /// 화면 터치시 작성 종료하는 메서드
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    /**
+     - Description: 화면 터치시 키보드 내리는 Extension
+     */
+    @objc
+    func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
