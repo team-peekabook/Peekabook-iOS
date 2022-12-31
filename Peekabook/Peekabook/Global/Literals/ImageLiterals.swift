070e59e6f982ecd87df57593bc8c4d67f9d7df03
devxsby <devxsby@gmail.com>
2022-12-31 22:53:39 +0900

    [Feat] #1- Image Literals 추가

---
+++ Peekabook/Peekabook/Global/Literals/ImageLiterals.swift
@@ -0,0 +1,22 @@
+//
+//  ImageLiterals.swift
+//  Peekabook
+//
+//  Created by devxsby on 2022/12/31.
+//
+
+import UIKit
+
+struct ImageLiterals {
+    
+    struct TabBar {
+        static let bookshelf = UIImage(named: "icn_bookshelf")
+        static let bookshelfSelected = UIImage(named: "icn_bookshelf_fill")
+        
+        static let recommend = UIImage(named: "icn_recom")
+        static let recommendSelected = UIImage(named: "icn_recom_fill")
+        
+        static let myPage = UIImage(named: "icn_my")
+        static let myPageSelected = UIImage(named: "icn_my_fill")
+    }
+}
