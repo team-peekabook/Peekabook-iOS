37c9d3c718623181213a62041fd6de75296a3cd8
devxsby <devxsby@gmail.com>
2022-12-31 22:39:00 +0900

    [Feat] #1- Color Literals 추가

---
+++ Peekabook/Peekabook/Global/Literals/ColorLiterals.swift
@@ -0,0 +1,23 @@
+//
+//  ColorLiterals.swift
+//  Peekabook
+//
+//  Created by devxsby on 2022/12/31.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    @nonobjc class var peekaRed: UIColor {
+        return UIColor(red: 0.565, green: 0.169, blue: 0.129, alpha: 1)
+    }
+    
+    @nonobjc class var peekaGray1: UIColor {
+        return UIColor(red: 0.642, green: 0.642, blue: 0.642, alpha: 1)
+    }
+    
+    @nonobjc class var peekaBeige: UIColor {
+        return UIColor(red: 0.929, green: 0.922, blue: 0.875, alpha: 1)
+    }
+}
