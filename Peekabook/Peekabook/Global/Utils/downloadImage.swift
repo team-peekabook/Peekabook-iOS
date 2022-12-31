333e704c65f5a7d42296f04e7c2d7e7744ea2436
devxsby <devxsby@gmail.com>
2022-12-31 22:58:26 +0900

    [Feat] #1- downloadImage util 추가

---
+++ Peekabook/Peekabook/Global/Utils/downloadImage.swift
@@ -0,0 +1,28 @@
+//
+//  downloadImage.swift
+//  Peekabook
+//
+//  Created by devxsby on 2022/12/31.
+//
+
+import UIKit
+
+import Kingfisher
+
+extension NSObject {
+    func downloadImage(with urlString: String, imageCompletionHandler: @escaping (UIImage?) -> Void) {
+        guard let url = URL.init(string: urlString) else {
+            return  imageCompletionHandler(nil)
+        }
+        let resource = ImageResource(downloadURL: url)
+        
+        KingfisherManager.shared.retrieveImage(with: resource, options: nil, progressBlock: nil) { result in
+            switch result {
+            case .success(let value):
+                imageCompletionHandler(value.image)
+            case .failure:
+                imageCompletionHandler(nil)
+            }
+        }
+    }
+}
