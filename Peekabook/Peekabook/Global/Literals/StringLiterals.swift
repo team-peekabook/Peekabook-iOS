7d826c9603791c36f26999d884c72ec2cb06d8d3
devxsby <devxsby@gmail.com>
2022-12-31 23:11:13 +0900

    [Feat] #1- Alert 관련 util 추가

--- Peekabook/Peekabook/Global/Literals/StringLiterals.swift
+++ Peekabook/Peekabook/Global/Literals/StringLiterals.swift
@@ -9,6 +9,11 @@ import UIKit
 
 struct I18N {
     
+    struct Alert {
+        static let error = "에러"
+        static let networkError = "네트워크 오류가 발생하였습니다."
+    }
+    
     struct Tabbar {
         static let bookshelf = "책장"
         static let recommend = "추천 "
