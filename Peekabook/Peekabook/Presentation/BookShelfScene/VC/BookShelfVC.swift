07ce5971b371234e3cf2f1b8a8c879a793867f7c
devxsby <devxsby@gmail.com>
2022-12-31 20:40:26 +0900

    [Chore] #1- 기본 탭 바 뷰컨트롤러 세팅

--- Peekabook/Peekabook/Presentation/BookShelfScene/VC/BookShelfVC.swift
+++ Peekabook/Peekabook/Presentation/BookShelfScene/VC/BookShelfVC.swift
@@ -7,12 +7,25 @@
 
 import UIKit
 
+import SnapKit
+import Then
+
+import Moya
+
 final class BookShelfVC: UIViewController {
+    
+    // MARK: - Properties
+
+    // MARK: - UI Components
+
+    // MARK: - View Life Cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .lightGray
+        view.backgroundColor = UIColor(red: 0.929, green: 0.922, blue: 0.875, alpha: 1)
     }
-
-
 }
+
+// MARK: - UI & Layout
+
+// MARK: - Methods
