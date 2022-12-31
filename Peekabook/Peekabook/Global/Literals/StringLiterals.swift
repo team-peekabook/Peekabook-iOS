a8f05c5e19015faf42e7179d073d9f2474ef8213
devxsby <devxsby@gmail.com>
2022-12-31 22:53:21 +0900

    [Feat] #1- String Literals 추가

---
+++ Peekabook/Peekabook/Global/Literals/StringLiterals.swift
@@ -0,0 +1,17 @@
+//
+//  StringLiterals.swift
+//  Peekabook
+//
+//  Created by devxsby on 2022/12/31.
+//
+
+import UIKit
+
+struct I18N {
+    
+    struct Tabbar {
+        static let bookshelf = "책장"
+        static let recommend = "추천 "
+        static let mypage = "MY"
+    }
+}
