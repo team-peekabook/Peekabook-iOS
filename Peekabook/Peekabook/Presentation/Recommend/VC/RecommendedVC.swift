e69dfd67f143a5a31e8d3629805fc1a408842cf1
6uohul <kiy010604@naver.com>
2023-01-03 02:38:18 +0900

    [Feat] #7- 추천받은 책 뷰 추가

---
+++ Peekabook/Peekabook/Presentation/Recommend/VC/RecommendedVC.swift
@@ -0,0 +1,16 @@
+//
+//  RecommendedVC.swift
+//  Peekabook
+//
+//  Created by 김인영 on 2023/01/03.
+//
+
+import UIKit
+
+class RecommendedVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .peekaGray1
+    }
+}
