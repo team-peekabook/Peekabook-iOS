c589c3b138e7c1fe7725f7f084b1bce3245263ed
devxsby <devxsby@gmail.com>
2023-01-02 20:07:19 +0900

    [Feat] #3- 이미지 리터럴 추가

--- Peekabook/Peekabook/Global/Literals/ImageLiterals.swift
+++ Peekabook/Peekabook/Global/Literals/ImageLiterals.swift
@@ -19,4 +19,15 @@ struct ImageLiterals {
         static let myPage = UIImage(named: "icn_my")
         static let myPageSelected = UIImage(named: "icn_my_fill")
     }
+    
+    struct Icn {
+        static let addBook = UIImage(named: "icn_addBook")
+        static let back = UIImage(named: "icn_back")
+        static let check = UIImage(named: "icn_check")
+        static let close = UIImage(named: "icn_close")
+        static let delete = UIImage(named: "icn_delete")
+        static let edit = UIImage(named: "icn_edit")
+        static let friend = UIImage(named: "icn_friend")
+        static let notification = UIImage(named: "icn_notification")
+    }
 }
