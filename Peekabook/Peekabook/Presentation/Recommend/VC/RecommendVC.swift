e023eb4b708d620c4c808081989c2f39b3f105d8
6uohul <kiy010604@naver.com>
2023-01-03 00:21:23 +0900

    [Feat] #7- 헤더 뷰 레이아웃 구현

--- Peekabook/Peekabook/Presentation/Recommend/VC/RecommendVC.swift
+++ Peekabook/Peekabook/Presentation/Recommend/VC/RecommendVC.swift
@@ -18,6 +18,11 @@ final class RecommendVC: UIViewController {
 
     // MARK: - UI Components
     
+    private let headerView = UIView()
+    private let logoImage = UIImageView().then {
+        $0.image = UIImage(named: "peekabook_logo")
+    }
+    
     // MARK: - View Life Cycle
 
     override func viewDidLoad() {
@@ -36,7 +41,19 @@ extension RecommendVC {
     }
     
     private func setLayout() {
+        view.addSubview(headerView)
+        headerView.addSubview(logoImage)
         
+        headerView.snp.makeConstraints {
+            $0.top.leading.trailing.equalTo(view.safeAreaLayoutGuide)
+            $0.height.equalTo(52)
+        }
+        logoImage.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalToSuperview().offset(20)
+            $0.width.equalTo(150)
+            $0.height.equalTo(18)
+        }
     }
 }
 
