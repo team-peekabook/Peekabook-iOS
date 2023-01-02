036e8a9e042b7e60458af06fa420b2e5e307b28f
devxsby <devxsby@gmail.com>
2023-01-02 20:01:03 +0900

    [Feat] #3- 컬러 리터럴 추가

--- Peekabook/Peekabook/Global/Literals/ColorLiterals.swift
+++ Peekabook/Peekabook/Global/Literals/ColorLiterals.swift
@@ -13,11 +13,35 @@ extension UIColor {
         return UIColor(red: 0.565, green: 0.169, blue: 0.129, alpha: 1)
     }
     
+    @nonobjc class var peekaRed_60: UIColor {
+        return UIColor(red: 0.565, green: 0.169, blue: 0.129, alpha: 0.6)
+    }
+    
+    @nonobjc class var peekaWhite: UIColor {
+        return UIColor(red: 1, green: 1, blue: 1, alpha: 1)
+    }
+    
+    @nonobjc class var peekaWhite_60: UIColor {
+        return UIColor(red: 1, green: 1, blue: 1, alpha: 0.6)
+    }
+    
     @nonobjc class var peekaGray1: UIColor {
         return UIColor(red: 0.642, green: 0.642, blue: 0.642, alpha: 1)
     }
     
+    @nonobjc class var peekaGray2: UIColor {
+        return UIColor(red: 0.558, green: 0.558, blue: 0.558, alpha: 1)
+    }
+    
+    @nonobjc class var peekaGray2_60: UIColor {
+        return UIColor(red: 0.558, green: 0.558, blue: 0.558, alpha: 0.6)
+    }
+    
     @nonobjc class var peekaBeige: UIColor {
         return UIColor(red: 0.929, green: 0.922, blue: 0.875, alpha: 1)
+    }
+    
+    @nonobjc class var peekaLightBeige: UIColor {
+        return UIColor(red: 0.996, green: 0.993, blue: 0.979, alpha: 1)
     }
 }
