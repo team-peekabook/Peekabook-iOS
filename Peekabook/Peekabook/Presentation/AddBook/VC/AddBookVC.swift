218baed4a5ea458b9a5dab5b3f61d736fe441a6d
dudu <329_143@naver.com>
2023-01-03 15:13:41 +0900

    [Feat] #9 - 한마디 박스 레이아웃

--- Peekabook/Peekabook/Presentation/AddBook/VC/AddBookVC.swift
+++ Peekabook/Peekabook/Presentation/AddBook/VC/AddBookVC.swift
@@ -54,6 +54,37 @@ final class AddBookVC: UIViewController {
         $0.textColor = .peekaRed
     }
     
+    private let commentBox = UIView().then {
+        $0.backgroundColor = .white
+        $0.layer.borderWidth = 2
+        $0.layer.borderColor = UIColor(red: 0.565, green: 0.169, blue: 0.129, alpha: 1).cgColor
+    }
+    
+    private let commentHeader = UIView().then {
+        $0.backgroundColor = .peekaRed
+    }
+    
+    private let commentLabel = UILabel().then {
+        $0.text = "한 마디"
+        $0.font = .h1
+        $0.textColor = .white
+    }
+    
+    private let commentViewPlaceholder = "한 마디를 남겨주세요."
+    private lazy var commentView = UITextView().then {
+        $0.font = .h2
+        $0.textColor = .peekaGray1
+        $0.text = commentViewPlaceholder
+        $0.layer.borderColor = UIColor.black.cgColor
+        $0.backgroundColor = .clear
+    }
+    
+    lazy var commentMaxLabel = UILabel().then {
+        $0.text = "0/200"
+        $0.font = .h2
+        $0.textColor = .peekaGray2
+    }
+    
     // MARK: - View Life Cycle
 
     override func viewDidLoad() {
@@ -82,8 +113,16 @@ extension AddBookVC {
             headerView.addSubview($0)
         }
         
-        [bookImgView, nameLabel, authorLabel].forEach {
+        [bookImgView, nameLabel, authorLabel, commentBox, commentMaxLabel].forEach {
             containerView.addSubview($0)
+        }
+        
+        [commentHeader, commentView].forEach {
+            commentBox.addSubview($0)
+        }
+        
+        [commentLabel].forEach {
+            commentHeader.addSubview($0)
         }
         
         containerView.snp.makeConstraints { make in
@@ -125,6 +164,37 @@ extension AddBookVC {
             $0.centerX.equalToSuperview()
         }
         
+        commentBox.snp.makeConstraints {
+            $0.top.equalTo(authorLabel.snp.bottom).offset(16)
+            $0.centerX.equalToSuperview()
+            $0.width.equalTo(335)
+            $0.height.equalTo(229)
+        }
+        
+        commentHeader.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.centerX.equalToSuperview()
+            $0.width.equalToSuperview()
+            $0.height.equalTo(36)
+        }
+        
+        commentLabel.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalToSuperview().offset(14)
+        }
+        
+        commentView.snp.makeConstraints {
+            $0.top.equalTo(commentHeader.snp.bottom).offset(10)
+//            $0.centerX.equalToSuperview()
+            $0.leading.equalTo(commentLabel)
+            $0.width.equalTo(307)
+            $0.height.equalTo(169)
+        }
+        
+        commentMaxLabel.snp.makeConstraints {
+            $0.top.equalTo(commentBox.snp.bottom).offset(8)
+            $0.trailing.equalTo(commentBox.snp.trailing)
+        }
     }
     
     @objc private func popToSearchView() {
