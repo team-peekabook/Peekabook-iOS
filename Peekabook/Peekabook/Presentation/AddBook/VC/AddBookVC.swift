75178c63c05cdc8b6e84242bbd416e467d0276b9
dudu <329_143@naver.com>
2023-01-03 14:58:34 +0900

    [Feat] #9 - 헤더뷰 레이아웃

--- Peekabook/Peekabook/Presentation/AddBook/VC/AddBookVC.swift
+++ Peekabook/Peekabook/Presentation/AddBook/VC/AddBookVC.swift
@@ -18,12 +18,35 @@ final class AddBookVC: UIViewController {
 
     // MARK: - UI Components
     
+    private let headerView = UIView().then {
+        $0.backgroundColor = .clear
+    }
+    
+    private let touchBackButton = UIButton().then {
+        $0.addTarget(AddBookVC.self, action: #selector(popToSearchView), for: .touchUpInside)
+    }
+    
+    private let headerTitle = UILabel().then {
+        $0.text = "책 등록하기"
+        $0.font = .h3
+        $0.textColor = .peekaRed
+    }
+    
+    private let touchCheckButton = UIButton().then {
+        $0.addTarget(AddBookVC.self, action: #selector(pushToDetailView), for: .touchUpInside)
+    }
+    
+    private lazy var containerView = UIScrollView().then {
+        $0.backgroundColor = .clear
+    }
+    
     // MARK: - View Life Cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
         setUI()
         setLayout()
+        configButton()
     }
 }
 
@@ -36,7 +59,52 @@ extension AddBookVC {
     }
     
     private func setLayout() {
+        [containerView, headerView].forEach {
+            view.addSubview($0)
+        }
         
+        [touchBackButton, headerTitle, touchCheckButton].forEach {
+            headerView.addSubview($0)
+        }
+        
+        containerView.snp.makeConstraints { make in
+            make.top.equalTo(headerView.snp.bottom)
+            make.leading.trailing.bottom.equalTo(view.safeAreaLayoutGuide)
+        }
+        
+        headerView.snp.makeConstraints { make in
+            make.top.leading.trailing.equalTo(view.safeAreaLayoutGuide)
+            make.height.equalTo(52)
+        }
+        
+        touchBackButton.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.leading.equalToSuperview()
+        }
+        
+        headerTitle.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
+        
+        touchCheckButton.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.trailing.equalToSuperview()
+        }
+        
+    }
+    
+    @objc private func popToSearchView() {
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+    // TODO: - push 함수 작성
+    @objc private func pushToDetailView() {
+        
+    }
+    
+    private func configButton() {
+        touchBackButton.setImage(ImageLiterals.Icn.back, for: .normal)
+        touchCheckButton.setImage(ImageLiterals.Icn.check, for: .normal)
     }
 }
 
