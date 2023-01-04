6bbe49db4dd639ee3bf2ad73026bb07e706de47c
dudu <329_143@naver.com>
2023-01-04 18:26:31 +0900

    [Chore] #11 - 코드리뷰반영 (StringLiterals)

--- Peekabook/Peekabook/Global/Literals/StringLiterals.swift
+++ Peekabook/Peekabook/Global/Literals/StringLiterals.swift
@@ -24,4 +24,8 @@ struct I18N {
         static let cancel = "취소하기"
         static let recommend = "추천하기"
     }
+    
+    struct PlaceHolder {
+        static let recommend = "추천사를 적어주세요."
+    }
 }
