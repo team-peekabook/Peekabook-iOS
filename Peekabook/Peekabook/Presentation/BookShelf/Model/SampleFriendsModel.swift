9e07bdcf771b29915ecd89d49255a1171248e4d6
devxsby <devxsby@gmail.com>
2023-01-05 15:12:32 +0900

    [Chore] #8- SampleFriendsModel 수정

--- Peekabook/Peekabook/Presentation/BookShelf/Model/SampleFriendsModel.swift
+++ Peekabook/Peekabook/Presentation/BookShelf/Model/SampleFriendsModel.swift
@@ -16,9 +16,9 @@ extension SampleFriendsModel {
     static var data: [SampleFriendsModel] {
         return [
             SampleFriendsModel(name: "김인영", profileImage: ImageLiterals.Sample.profile1!),
-            SampleFriendsModel(name: "고두영", profileImage: ImageLiterals.Sample.profile2!),
+            SampleFriendsModel(name: "고두영두영", profileImage: ImageLiterals.Sample.profile2!),
             SampleFriendsModel(name: "박현정", profileImage: ImageLiterals.Sample.profile3!),
-            SampleFriendsModel(name: "한지우", profileImage: ImageLiterals.Sample.profile4!),
+            SampleFriendsModel(name: "한지우지우지우", profileImage: ImageLiterals.Sample.profile4!),
             SampleFriendsModel(name: "조예슬", profileImage: ImageLiterals.Sample.profile5!),
             SampleFriendsModel(name: "이영주", profileImage: ImageLiterals.Sample.profile6!),
             SampleFriendsModel(name: "이현우", profileImage: ImageLiterals.Sample.profile1!),
