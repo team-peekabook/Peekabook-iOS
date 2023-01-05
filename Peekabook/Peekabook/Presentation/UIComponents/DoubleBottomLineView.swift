f3c441c03920e3c3ebb3fb58f3be4820b426f614
devxsby <devxsby@gmail.com>
2023-01-05 17:57:30 +0900

    [Feat] #8- 라인이 2줄인 뷰 커스텀 uiview 생성

---
+++ Peekabook/Peekabook/Presentation/UIComponents/DoubleBottomLineView.swift
@@ -0,0 +1,8 @@
+//
+//  DoubleBottomLineView.swift
+//  Peekabook
+//
+//  Created by devxsby on 2023/01/05.
+//
+
+import Foundation
---
+++ Peekabook/Peekabook/Presentation/UIComponents/DoubleHeaderLineView.swift
@@ -0,0 +1,8 @@
+//
+//  DoubleHeaderLineView.swift
+//  Peekabook
+//
+//  Created by devxsby on 2023/01/05.
+//
+
+import Foundation
