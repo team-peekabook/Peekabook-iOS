93cdc8f60b2e057aaff8dbab0edc05b387064e45
dudu <329_143@naver.com>
2023-01-06 22:58:01 +0900

    [Feat] #25 - 바코드 스캔뷰 헤더 배경 적용

--- Peekabook/Peekabook/Presentation/AddBook/VC/BarcodeViewController.swift
+++ Peekabook/Peekabook/Presentation/AddBook/VC/BarcodeViewController.swift
@@ -18,18 +18,48 @@ final class BarcodeViewController: BarcodeScannerViewController {
         $0.textColor = .black
         $0.font = .systemFont(ofSize: 20, weight: .bold)
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        let viewController = BarcodeScannerViewController()
+//        viewController.headerViewController.closeButton.setImage(UIImage(named: "search"), for: .normal)
+//        viewController.headerViewController.closeButton.setTitle("", for: .normal)
+//        viewController.headerViewController.closeButton.setImage(ImageLiterals.Icn.close, for: .normal)
+//        viewController.headerViewController.closeButton.setTitleColor(.red, for: .normal)
+//        viewController.headerViewController.titleLabel.text = "책 검색하기"
+        viewController.headerViewController.titleLabel.text = "example"
+        
+        viewController.cameraViewController.focusView.layer.borderWidth = 2
+        viewController.cameraViewController.focusView.layer.borderColor = UIColor(red: 0.565, green: 0.169, blue: 0.129, alpha: 1).cgColor
+        
+        viewController.modalPresentationStyle = .fullScreen
+        viewController.cameraViewController.barCodeFocusViewType = .twoDimensions // 바코드 규격 스타일
+        viewController.cameraViewController.focusView.transform = CGAffineTransform(scaleX: 1.5, y: 1.3)
+        viewController.cameraViewController.flashButton.transform = CGAffineTransform(scaleX: 0, y: 0)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         
         let viewController = BarcodeScannerViewController()
-        viewController.headerViewController.closeButton.setImage(UIImage(named: "search"), for: .normal)
-        viewController.headerViewController.closeButton.setTitle("", for: .normal)
-        viewController.headerViewController.titleLabel.text = "책 검색하기"
+//        viewController.headerViewController.closeButton.setImage(UIImage(named: "search"), for: .normal)
+//        viewController.headerViewController.closeButton.setTitle("", for: .normal)
+//        viewController.headerViewController.closeButton.setImage(ImageLiterals.Icn.close, for: .normal)
+//        viewController.headerViewController.closeButton.setTitleColor(.red, for: .normal)
+//        viewController.headerViewController.titleLabel.text = "책 검색하기"
+        viewController.headerViewController.titleLabel.text = "example"
+        
+        viewController.cameraViewController.focusView.layer.borderWidth = 2
+        viewController.cameraViewController.focusView.layer.borderColor = UIColor(red: 0.565, green: 0.169, blue: 0.129, alpha: 1).cgColor
+        
+        viewController.modalPresentationStyle = .fullScreen
+        viewController.cameraViewController.barCodeFocusViewType = .twoDimensions // 바코드 규격 스타일
+        viewController.cameraViewController.focusView.transform = CGAffineTransform(scaleX: 1.5, y: 1.3)
+        viewController.cameraViewController.flashButton.transform = CGAffineTransform(scaleX: 0, y: 0)
         
         codeDelegate = self
         setLayout()
+        dismissalDelegate = self
     }
 }
 
@@ -52,4 +82,10 @@ extension BarcodeViewController: BarcodeScannerCodeDelegate {
         nextVC.modalPresentationStyle = .fullScreen
         self.present(nextVC, animated: true, completion: nil)
     }
+}
+
+extension BarcodeViewController: BarcodeScannerDismissalDelegate {
+  func scannerDidDismiss(_ controller: BarcodeScannerViewController) {
+    controller.dismiss(animated: true, completion: nil)
+  }
 }
