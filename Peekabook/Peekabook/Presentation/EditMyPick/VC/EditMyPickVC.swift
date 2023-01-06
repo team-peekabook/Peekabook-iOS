246e9f49ee9ca5e0cddf6c6d8bec35c4fe40397f
devxsby <devxsby@gmail.com>
2023-01-06 11:39:23 +0900

    [Feat] #21- header view 추가

--- Peekabook/Peekabook/Presentation/EditMyPick/VC/EditMyPickVC.swift
+++ Peekabook/Peekabook/Presentation/EditMyPick/VC/EditMyPickVC.swift
@@ -17,7 +17,26 @@ final class EditMyPickVC: UIViewController {
     // MARK: - Properties
 
     // MARK: - UI Components
+    
+    private let naviContainerView = UIView()
 
+    private lazy var backButton = UIButton(type: .system).then {
+        $0.setImage(ImageLiterals.Icn.back, for: .normal)
+        $0.addTarget(self, action: #selector(backButtonDidTap), for: .touchUpInside)
+    }
+    
+    private lazy var completeButton = UIButton(type: .system).then {
+        $0.setImage(ImageLiterals.Icn.check, for: .normal)
+        $0.addTarget(self, action: #selector(completeButtonDidTap), for: .touchUpInside)
+    }
+    
+    private let descriptionLabel = UILabel().then {
+        $0.text = I18N.BookShelf.editPickDescription
+        $0.font = .s1
+        $0.textColor = .peekaRed
+        $0.textAlignment = .left
+    }
+    
     // MARK: - View Life Cycle
 
     override func viewDidLoad() {
@@ -25,16 +44,50 @@ final class EditMyPickVC: UIViewController {
         setUI()
         setLayout()
     }
+    
+    // MARK: - @objc Function
+    
+    @objc
+    private func backButtonDidTap() {
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+    @objc
+    private func completeButtonDidTap() {
+        print("confirmButtonDidTap")
+    }
 }
 
 // MARK: - UI & Layout
 extension EditMyPickVC {
     
     private func setUI() {
-        self.view.backgroundColor = .white
+        view.backgroundColor = .white
     }
     
     private func setLayout() {
+        view.addSubviews(naviContainerView)
+        naviContainerView.addSubviews(backButton, completeButton, descriptionLabel)
+        
+        naviContainerView.snp.makeConstraints { make in
+            make.top.leading.trailing.equalTo(view.safeAreaLayoutGuide)
+            make.height.equalTo(65)
+        }
+        
+        backButton.snp.makeConstraints { make in
+            make.top.equalToSuperview()
+            make.leading.equalToSuperview().offset(5)
+        }
+        
+        completeButton.snp.makeConstraints { make in
+            make.top.equalToSuperview()
+            make.trailing.equalToSuperview().inset(5)
+        }
+        
+        descriptionLabel.snp.makeConstraints { make in
+            make.top.equalTo(backButton.snp.bottom)
+            make.leading.equalToSuperview().offset(20)
+        }
     }
 }
 
