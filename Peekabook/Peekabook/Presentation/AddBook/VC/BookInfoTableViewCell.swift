ac13693e81c0ebbfdee6e41645634ccc90c50289
dudu <329_143@naver.com>
2023-01-06 12:00:49 +0900

    [Feat] #19 - 테이블뷰 레이아웃

--- Peekabook/Peekabook/Presentation/AddBook/VC/BookInfoTableViewCell.swift
+++ Peekabook/Peekabook/Presentation/AddBook/VC/BookInfoTableViewCell.swift
@@ -25,8 +25,11 @@ class BookInfoTableViewCell: UITableViewCell {
     
     // MARK: - UI Components
 
-    private let bookContainerView = UIView()
+    private let containerView = UIStackView()
+    private let imgContainerView = UIView()
     private let bookImgView = UIImageView()
+    private let labelContainerView = UIView()
+    private let addContainerView = UIView()
     private let bookTitleLabel = UILabel().then {
         $0.font = .h3
         $0.textColor = .peekaRed
@@ -58,7 +61,66 @@ class BookInfoTableViewCell: UITableViewCell {
 
 extension BookInfoTableViewCell {
     private func setLayout() {
+        self.backgroundColor = .white
+        containerView.addSubviews([
+            imgContainerView,
+            labelContainerView,
+            addContainerView
+        ])
         
+        imgContainerView.addSubviews(bookImgView)
+        
+        labelContainerView.addSubviews([
+            bookTitleLabel,
+            authorLabel
+        ])
+        
+        addContainerView.addSubviews([
+            addLabel,
+            addButton
+        ])
+        
+        imgContainerView.snp.makeConstraints { make in
+            make.top.leading.bottom.equalToSuperview()
+            make.width.equalTo(106)
+        }
+        
+        imgContainerView.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
+        
+        labelContainerView.snp.makeConstraints { make in
+            make.top.equalToSuperview()
+            make.leading.equalTo(imgContainerView.snp.trailing)
+            make.trailing.equalToSuperview()
+            make.height.equalTo(99)
+        }
+        
+        bookTitleLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(15)
+            make.leading.equalToSuperview().offset(15)
+        }
+        
+        authorLabel.snp.makeConstraints { make in
+            make.top.equalTo(bookTitleLabel.snp.bottom)
+            make.leading.equalTo(bookTitleLabel)
+        }
+        
+        addContainerView.snp.makeConstraints { make in
+            make.top.equalTo(labelContainerView.snp.bottom)
+            make.leading.equalTo(labelContainerView)
+            make.width.height.equalTo(labelContainerView)
+        }
+        
+        addButton.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.trailing.equalToSuperview().inset(30)
+        }
+        
+        addLabel.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.trailing.equalTo(addButton.snp.leading).inset(4)
+        }
     }
     
     func dataBind(model: BookInfoModel) {
