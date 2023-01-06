ba882b5de3ba9ca6b7b4769d3ed5c9f2e5f0ef44
dudu <329_143@naver.com>
2023-01-06 19:22:02 +0900

    [Fix] #25 - 주석 삭제 등 코드 수정

--- Peekabook/Peekabook/Presentation/AddBook/VC/BarcodeViewController.swift
+++ Peekabook/Peekabook/Presentation/AddBook/VC/BarcodeViewController.swift
@@ -5,50 +5,14 @@
 //  Created by 고두영 on 2023/01/06.
 //
 
-//import UIKit
-//
-//import BarcodeScanner
-//import SnapKit
-//import Then
-//
-//class BarcodeViewController: BarcodeScannerViewController {
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        view.backgroundColor = .white
-//
-//        let viewController = BarcodeScannerViewController()
-//        viewController.headerViewController.closeButton.setImage(ImageLiterals.Icn.close, for: .normal)
-//        viewController.headerViewController.closeButton.setTitle("", for: .normal)
-//        viewController.headerViewController.titleLabel.text = "책 검색하기"
-//
-//        codeDelegate = self
-//    }
-//}
-//
-//extension BarcodeViewController: BarcodeScannerCodeDelegate {
-//    func scanner(_ controller: BarcodeScannerViewController, didCaptureCode code: String, type: String) {
-//      print("Barcode Data: \(code)")
-//      print("Symbology Type: \(type)")
-//        let nextVC =  AddBookVC()
-//        nextVC.modalPresentationStyle = .fullScreen
-//        self.present(nextVC, animated: true, completion: nil)
-//    }
-//}
-
 import UIKit
+
 import BarcodeScanner
 import SnapKit
 import Then
 
-class BarcodeViewController: BarcodeScannerViewController, BarcodeScannerCodeDelegate {
-    func scanner(_ controller: BarcodeScannerViewController, didCaptureCode code: String, type: String) {
-      print("Barcode Data: \(code)")
-      print("Symbology Type: \(type)")
-        let nextVC = AddBookVC()
-        nextVC.modalPresentationStyle = .fullScreen
-        self.present(nextVC, animated: true, completion: nil)
-    }
+class BarcodeViewController: BarcodeScannerViewController {
+    
     
     private let labelA = UILabel().then {
         $0.text = "테스트입니다"
@@ -68,17 +32,25 @@ class BarcodeViewController: BarcodeScannerViewController, BarcodeScannerCodeDel
         codeDelegate = self
         setLayout()
     }
-
 }
 
 extension BarcodeViewController {
     private func setLayout() {
-//        view.addSubview(labelA)
         view.addSubview(labelA)
         
         labelA.snp.makeConstraints {
             $0.top.equalToSuperview().offset(500)
             $0.leading.equalToSuperview().offset(200)
         }
+    }
+}
+
+extension BarcodeViewController: BarcodeScannerCodeDelegate {
+    func scanner(_ controller: BarcodeScannerViewController, didCaptureCode code: String, type: String) {
+      print("Barcode Data: \(code)")
+      print("Symbology Type: \(type)")
+        let nextVC = AddBookVC()
+        nextVC.modalPresentationStyle = .fullScreen
+        self.present(nextVC, animated: true, completion: nil)
     }
 }
