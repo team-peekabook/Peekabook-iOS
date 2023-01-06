337ca558a78202cdbe515f3c408f354b47ad2e06
6uohul <kiy010604@naver.com>
2023-01-07 03:06:15 +0900

    [Feat] #30- Notification 모델 추가

---
+++ Peekabook/Peekabook/Presentation/MyNotification/NotificationModel.swift
@@ -0,0 +1,15 @@
+//
+//  NotificationModel.swift
+//  Peekabook
+//
+//  Created by 김인영 on 2023/01/07.
+//
+
+import UIKit
+
+struct NotificationModel {
+    let image : UIImage!
+    let notiLabel : String
+    let bookName : String?
+    let date : String
+}
