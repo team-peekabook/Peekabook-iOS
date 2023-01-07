10a621a72d2a55e6427c528f9119dd22323bceb7
devxsby <devxsby@gmail.com>
2023-01-07 23:02:16 +0900

    [Feat] #36- APIConstants BaseURL, Naver Search URL 추가

---
+++ Peekabook/Peekabook/Network/Base/APIConstants.swift
@@ -0,0 +1,16 @@
+//
+//  APIConstants.swift
+//  Peekabook
+//
+//  Created by devxsby on 2023/01/07.
+//
+
+import Foundation
+
+struct APIConstants {
+        
+    static let baseURL: String = "https://"
+    
+    static let searchURL: String = "https://openapi.naver.com/v1/search/book"
+}
+
