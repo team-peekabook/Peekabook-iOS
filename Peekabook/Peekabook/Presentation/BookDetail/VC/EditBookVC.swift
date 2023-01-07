e849775474f70c2ebabcf309267382791fa4b67f
dudu <329_143@naver.com>
2023-01-07 14:17:06 +0900

    [Feat] #37 - 헤더뷰 레이아웃

--- Peekabook/Peekabook/Presentation/BookDetail/VC/EditBookVC.swift
+++ Peekabook/Peekabook/Presentation/BookDetail/VC/EditBookVC.swift
@@ -17,6 +17,31 @@ final class EditBookVC: UIViewController {
     // MARK: - Properties
 
     // MARK: - UI Components
+    
+    private let headerView = UIView().then {
+        $0.backgroundColor = .clear
+    }
+    
+    private lazy var touchBackButton = UIButton().then {
+        $0.addTarget(self, action: #selector(touchBackButtonDidTap), for: .touchUpInside)
+    }
+    
+    private let headerTitle = UILabel().then {
+        $0.text = "책 수정하기"
+        $0.font = .h3
+        $0.textColor = .peekaRed
+    }
+    
+    private let touchCheckButton = UIButton().then {
+        $0.setTitle("완료", for: .normal)
+        $0.titleLabel!.font = .h4
+        $0.setTitleColor(.peekaRed, for: .normal)
+        $0.addTarget(AddBookVC.self, action: #selector(touchCheckButtonDidTap), for: .touchUpInside)
+    }
+    
+    private lazy var containerView = UIScrollView().then {
+        $0.backgroundColor = .clear
+    }
 
     // MARK: - View Life Cycle
 
@@ -24,6 +49,7 @@ final class EditBookVC: UIViewController {
         super.viewDidLoad()
         setUI()
         setLayout()
+        configButton()
     }
 }
 
@@ -35,7 +61,56 @@ extension EditBookVC {
     }
     
     private func setLayout() {
+        [containerView, headerView].forEach {
+            view.addSubview($0)
+        }
+        
+        [touchBackButton, headerTitle, touchCheckButton].forEach {
+            headerView.addSubview($0)
+        }
+        
+        containerView.snp.makeConstraints { make in
+            make.top.equalTo(headerView.snp.bottom)
+            make.leading.trailing.bottom.equalTo(view.safeAreaLayoutGuide)
+        }
+        
+        headerView.snp.makeConstraints { make in
+            make.top.leading.trailing.equalTo(view.safeAreaLayoutGuide)
+            make.height.equalTo(52)
+        }
+        
+        touchBackButton.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.leading.equalToSuperview()
+        }
+        
+        headerTitle.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
+        
+        touchCheckButton.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.trailing.equalToSuperview().inset(11)
+            make.width.height.equalTo(48)
+        }
     }
 }
 
 // MARK: - Methods
+
+extension EditBookVC {
+    // TODO: - 바코드 스캔뷰로 다시 가게 해야함
+    // 현재는 홈뷰로 가는 상황
+    @objc private func touchBackButtonDidTap() {
+        self.presentingViewController?.presentingViewController?.dismiss(animated: true, completion: nil)
+    }
+    
+    // TODO: - push 함수 작성 필요
+    @objc private func touchCheckButtonDidTap() {
+        // doSomething()
+    }
+    
+    private func configButton() {
+        touchBackButton.setImage(ImageLiterals.Icn.back, for: .normal)
+    }
+}
