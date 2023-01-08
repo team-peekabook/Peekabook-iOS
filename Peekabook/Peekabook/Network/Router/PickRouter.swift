cb859af2930f584812695da2bd494656d6d14dd3
devxsby <devxsby@gmail.com>
2023-01-09 00:13:59 +0900

    [Feat] #48- PickRouter sample 코드 추가

--- Peekabook/Peekabook/Network/Router/PickRouter.swift
+++ Peekabook/Peekabook/Network/Router/PickRouter.swift
@@ -6,3 +6,40 @@
 //
 
 import Foundation
+
+import Moya
+
+enum PickRouter {
+    case sample
+}
+
+extension PickRouter: TargetType {
+    var baseURL: URL {
+        return URL(string: URLConstant.baseURL)!
+    }
+    
+    var path: String {
+        switch self {
+        case .sample:
+            return URLConstant.pick
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .sample:
+            return .get
+        }
+    }
+    
+    var task: Moya.Task {
+        switch self {
+        case .sample:
+            return .requestPlain
+        }
+    }
+    
+    var headers: [String: String]? {
+        return NetworkConstant.defaultHeader
+    }
+}
