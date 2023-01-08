bf7cc9da00beb8ca813c0b4de4cea0c1830d938a
devxsby <devxsby@gmail.com>
2023-01-09 00:15:58 +0900

    [Feat] #48- AlarmRouter sample 코드 추가

--- Peekabook/Peekabook/Network/Router/AlarmRouter.swift
+++ Peekabook/Peekabook/Network/Router/AlarmRouter.swift
@@ -6,3 +6,40 @@
 //
 
 import Foundation
+
+import Moya
+
+enum AlarmRouter {
+    case sample
+}
+
+extension AlarmRouter: TargetType {
+    var baseURL: URL {
+        return URL(string: URLConstant.baseURL)!
+    }
+    
+    var path: String {
+        switch self {
+        case .sample:
+            return URLConstant.alarm
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .sample:
+            return .get
+        }
+    }
+    
+    var task: Moya.Task {
+        switch self {
+        case .sample:
+            return .requestPlain
+        }
+    }
+    
+    var headers: [String: String]? {
+        return NetworkConstant.defaultHeader
+    }
+}
