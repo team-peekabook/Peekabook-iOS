03dc80f2fde4196a01dda89e86a79664a53049d0
devxsby <devxsby@gmail.com>
2023-01-09 00:14:36 +0900

    [Feat] #48- RecommendRouter sample 코드 추가

--- Peekabook/Peekabook/Network/Router/RecommendRouter.swift
+++ Peekabook/Peekabook/Network/Router/RecommendRouter.swift
@@ -6,3 +6,40 @@
 //
 
 import Foundation
+
+import Moya
+
+enum RecommendRouter {
+    case sample
+}
+
+extension RecommendRouter: TargetType {
+    var baseURL: URL {
+        return URL(string: URLConstant.baseURL)!
+    }
+    
+    var path: String {
+        switch self {
+        case .sample:
+            return URLConstant.recommend
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .sample:
+            return .get
+        }
+    }
+    
+    var task: Moya.Task {
+        switch self {
+        case .sample:
+            return .requestPlain
+        }
+    }
+    
+    var headers: [String: String]? {
+        return NetworkConstant.defaultHeader
+    }
+}
