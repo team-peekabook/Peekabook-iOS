ece824b15928d0ec80cd2c0d041e2935cc3e2d25
6uohul <kiy010604@naver.com>
2023-01-09 18:30:11 +0900

    [Chore] #44- 추천받은 뷰 컨벤션에 맞게 줄바꿈 수정

--- Peekabook/Peekabook/Presentation/Recommend/VC/RecommendedVC.swift
+++ Peekabook/Peekabook/Presentation/Recommend/VC/RecommendedVC.swift
@@ -12,23 +12,30 @@ class RecommendedVC: UIViewController {
     // MARK: - Properties
     
     private var recommendedDummy: [RecommendModel] = [
-        RecommendModel(image: ImageLiterals.Sample.book1,
-                       bookName: "아무튼, 여름",
-                       writer: "김신회",
-                       recommendedPersonImage: ImageLiterals.Sample.profile3,
-                       recommendedPerson: "김인영",
-                       memo: "‘추천사요약’ 을 쓸 건데 나는 이 책이 상당한 지식을 얻을 수 있는 기회를 제공한다고 생각합니다. 당신에게 추천해요!"),
-        RecommendModel(image: ImageLiterals.Sample.book1,
-                       bookName: "아무튼, 여름", writer: "김신회",
-                       recommendedPersonImage: ImageLiterals.Sample.profile2,
-                       recommendedPerson: "윤수빈",
-                       memo: "‘추천사요약’ 을 쓸 건데 나는 이 책이 상당한 지식을 얻을 수 있는 기회를 제공한다고 생각합니다. 당신에게 추천해요!"),
-        RecommendModel(image: ImageLiterals.Sample.book1,
-                       bookName: "아무튼, 여름",
-                       writer: "김신회",
-                       recommendedPersonImage: ImageLiterals.Sample.profile1,
-                       recommendedPerson: "두두",
-                       memo: "‘추천사요약’ 을 쓸 건데 나는 이 책이 상당한 지식을 얻을 수 있는 기회를 제공한다고 생각합니다. 당신에게 추천해요! ‘추천사요약’을 쓸 건데 나는 이 책이 상당한 지식을 얻을 수 있는 기회를 제공한다고 생각합니다. ‘추천사요약’을 쓸 건데 나는 이 책이 상당한 지식을 얻을 수 있는 기회를 제공한다고 생각합니다. ‘추천사요약’을 쓸 건데 나는 이 책이 후")
+        RecommendModel(
+            image: ImageLiterals.Sample.book1,
+            bookName: "아무튼, 여름",
+            writer: "김신회",
+            recommendedPersonImage: ImageLiterals.Sample.profile3,
+            recommendedPerson: "김인영",
+            memo: "‘추천사요약’ 을 쓸 건데 나는 이 책이 상당한 지식을 얻을 수 있는 기회를 제공한다고 생각합니다. 당신에게 추천해요!"
+        ),
+        RecommendModel(
+            image: ImageLiterals.Sample.book1,
+            bookName: "아무튼, 여름",
+            writer: "김신회",
+            recommendedPersonImage: ImageLiterals.Sample.profile3,
+            recommendedPerson: "윤수빈",
+            memo: "‘추천사요약’ 을 쓸 건데 나는 이 책이 상당한 지식을 얻을 수 있는 기회를 제공한다고 생각합니다. 당신에게 추천해요!"
+        ),
+        RecommendModel(
+            image: ImageLiterals.Sample.book1,
+            bookName: "아무튼, 여름",
+            writer: "김신회",
+            recommendedPersonImage: ImageLiterals.Sample.profile3,
+            recommendedPerson: "김인영",
+            memo: ""
+        ),
     ]
     
     // MARK: - UI Components
@@ -56,7 +63,10 @@ class RecommendedVC: UIViewController {
 
 extension RecommendedVC {
     private func registerCells() {
-        tableView.register(RecommendTVC.self, forCellReuseIdentifier: RecommendTVC.className)
+        tableView.register(
+            RecommendTVC.self,
+            forCellReuseIdentifier: RecommendTVC.className
+        )
     }
     
     private func setUI() {
@@ -78,16 +88,31 @@ extension RecommendedVC {
 
 extension RecommendedVC: UITableViewDelegate, UITableViewDataSource {
     
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    func tableView(
+        _ tableView: UITableView,
+        heightForRowAt indexPath: IndexPath
+    ) -> CGFloat {
         return 221
     }
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func tableView(
+        _ tableView: UITableView,
+        numberOfRowsInSection section: Int
+    ) -> Int {
         return recommendedDummy.count
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: RecommendTVC.className, for: indexPath) as? RecommendTVC else { return UITableViewCell() }
+    func tableView(
+        _ tableView: UITableView,
+        cellForRowAt indexPath: IndexPath
+    ) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(
+            withIdentifier: RecommendTVC.className,
+            for: indexPath
+        ) as? RecommendTVC
+        else {
+            return UITableViewCell()
+        }
         cell.dataBind(model: recommendedDummy[indexPath.item])
         return cell
     }
