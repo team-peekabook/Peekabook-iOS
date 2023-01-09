f389ec48d83695e62b78f9330cd8fc35d7b10a37
6uohul <kiy010604@naver.com>
2023-01-10 03:16:43 +0900

    [Chore] #63- 추천 탭 뷰 구분선 제거

--- Peekabook/Peekabook/Presentation/Recommend/VC/RecommendingVC.swift
+++ Peekabook/Peekabook/Presentation/Recommend/VC/RecommendingVC.swift
@@ -45,6 +45,7 @@ class RecommendingVC: UIViewController {
         $0.isScrollEnabled = true
         $0.allowsSelection = false
         $0.allowsMultipleSelection = false
+        $0.separatorStyle = .none
     }
     
     // MARK: - View Life Cycle
