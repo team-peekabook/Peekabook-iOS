f0820c22961c6dc3d4348a84e95fab529e90d448
dudu <329_143@naver.com>
2023-01-09 20:28:29 +0900

    [Add] #53 - 책검색vc에 엠티뷰 추가

--- Peekabook/Peekabook/Presentation/AddBook/VC/BookSearchVC.swift
+++ Peekabook/Peekabook/Presentation/AddBook/VC/BookSearchVC.swift
@@ -19,6 +19,7 @@ final class BookSearchVC: UIViewController {
     // MARK: - UI Components
     
     private let headerView = UIView()
+    private let containerView = UIView()
     
     private lazy var cancelButton = UIButton().then {
         $0.addTarget(self, action: #selector(cancelButtonDidTap), for: .touchUpInside)
@@ -50,7 +51,7 @@ final class BookSearchVC: UIViewController {
     private lazy var bookTableView: UITableView = {
         let tableView = UITableView()
         tableView.showsVerticalScrollIndicator = true
-        tableView.backgroundColor = .peekaBeige
+        tableView.backgroundColor = .red
         tableView.translatesAutoresizingMaskIntoConstraints = false
         tableView.delegate = self
         tableView.dataSource = self
@@ -61,8 +62,26 @@ final class BookSearchVC: UIViewController {
         BookInfoModel(image: ImageLiterals.Sample.book1, title: "아무튼, 여름", author: "김신회"),
         BookInfoModel(image: ImageLiterals.Sample.book2, title: "아무튼, 두영", author: "김인영"),
         BookInfoModel(image: ImageLiterals.Sample.book3, title: "아무튼, 수빈", author: "고두영"),
+        BookInfoModel(image: ImageLiterals.Sample.book4, title: "아무튼, 인영", author: "윤수빈"),
+        BookInfoModel(image: ImageLiterals.Sample.book4, title: "아무튼, 인영", author: "윤수빈"),
+        BookInfoModel(image: ImageLiterals.Sample.book4, title: "아무튼, 인영", author: "윤수빈"),
         BookInfoModel(image: ImageLiterals.Sample.book4, title: "아무튼, 인영", author: "윤수빈")
     ]
+    
+    // emptyView elements
+    
+    let emptyView = UIView()
+    private let emptyImgView = UIImageView().then {
+        $0.image = ImageLiterals.Icn.empty
+    }
+    
+    private let emptyLabel = UILabel().then {
+        $0.font = .h2
+        $0.text = I18N.BookSearch.empty
+        $0.textColor = .peekaRed_60
+        $0.numberOfLines = 2
+        $0.textAlignment = .center
+    }
 
     // MARK: - View Life Cycle
 
@@ -81,55 +100,88 @@ extension BookSearchVC {
         self.view.backgroundColor = .peekaBeige
         headerView.backgroundColor = .clear
         headerLineView.backgroundColor = .peekaRed
+        emptyView.backgroundColor = .clear
         
         cancelButton.setImage(ImageLiterals.Icn.close, for: .normal)
         searchButton.setImage(ImageLiterals.Icn.search, for: .normal)
     }
     
     private func setLayout() {
-        [headerView, searchField, headerLineView, bookTableView].forEach {
+        [headerView, containerView].forEach {
             view.addSubview($0)
         }
         
-        [cancelButton, headerTitleLabel].forEach {
+        [bookTableView].forEach {
+            containerView.addSubview($0)
+        }
+        
+        [cancelButton, headerTitleLabel, searchField, headerLineView].forEach {
             headerView.addSubview($0)
         }
         
         headerView.snp.makeConstraints { make in
             make.top.leading.trailing.equalTo(view.safeAreaLayoutGuide)
-            make.height.equalTo(52)
+            make.height.equalTo(110)
+        }
+    
+        containerView.snp.makeConstraints { make in
+            make.top.equalTo(headerView.snp.bottom)
         }
         
         cancelButton.snp.makeConstraints { make in
-            make.centerY.equalToSuperview()
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(2)
             make.trailing.equalToSuperview().inset(11)
             make.width.height.equalTo(48)
         }
         
         headerTitleLabel.snp.makeConstraints { make in
-            make.center.equalToSuperview()
+            make.centerX.equalToSuperview()
+            make.centerY.equalTo(cancelButton)
         }
         
         headerLineView.snp.makeConstraints { make in
-            make.top.equalTo(headerView.snp.bottom)
+            make.top.equalTo(headerTitleLabel.snp.bottom).offset(14)
             make.leading.equalToSuperview().offset(20)
             make.trailing.equalToSuperview().inset(20)
             make.height.equalTo(2)
         }
         
         searchField.snp.makeConstraints { make in
-            make.top.equalTo(headerView.snp.bottom).offset(18)
+            make.top.equalTo(headerLineView.snp.bottom).offset(16)
             make.leading.trailing.equalToSuperview().inset(20)
             make.centerX.equalToSuperview()
             make.height.equalTo(40)
         }
         
         bookTableView.snp.makeConstraints { make in
-            make.top.equalTo(searchField.snp.bottom).offset(24)
+            make.top.equalToSuperview().offset(24)
             make.leading.equalTo(view.safeAreaLayoutGuide).offset(20)
             make.trailing.equalTo(view.safeAreaLayoutGuide).inset(20)
             make.bottom.equalToSuperview()
             make.height.equalTo(128 * bookInfoList.count)
+        }
+        
+        // emptyView Layout
+        
+        view.addSubview(emptyView)
+        [emptyImgView, emptyLabel].forEach {
+            emptyView.addSubview($0)
+        }
+        
+        emptyView.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+            make.width.equalTo(247)
+            make.height.equalTo(96)
+        }
+        
+        emptyImgView.snp.makeConstraints { make in
+            make.top.equalToSuperview()
+            make.centerX.equalToSuperview()
+        }
+        
+        emptyLabel.snp.makeConstraints { make in
+            make.top.equalTo(emptyImgView.snp.bottom).offset(8)
+            make.centerX.equalToSuperview()
         }
     }
     
@@ -166,6 +218,12 @@ extension BookSearchVC: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if self.bookInfoList.isEmpty == true {
+            self.emptyView.isHidden = false
+        } else {
+            self.emptyView.isHidden = true
+        }
+        
         guard let bookCell = tableView.dequeueReusableCell(
             withIdentifier: BookInfoTableViewCell.identifier,
             for: indexPath) as? BookInfoTableViewCell
