54e006118e881bbc08b9948e411104315fc48fcd
devxsby <devxsby@gmail.com>
2023-01-09 13:07:40 +0900

    [Chore] #21- SampleEditPickModel 수정

--- Peekabook/Peekabook/Presentation/EditMyPick/Model/SampleEditPickModel.swift
+++ Peekabook/Peekabook/Presentation/EditMyPick/Model/SampleEditPickModel.swift
@@ -10,37 +10,37 @@ import UIKit
 struct SampleEditPickModel {
 //    var bookId: Int // 서버 연결 시 사용하려고 미리 넣어놓았습니다.
     var bookImage: UIImage
-    var countLabel: Int?
+    var countLabel: Int
 }
 
 extension SampleEditPickModel {
     static var data: [SampleEditPickModel] {
         return [
-            SampleEditPickModel(bookImage: ImageLiterals.Sample.book1!, countLabel: nil),
+            SampleEditPickModel(bookImage: ImageLiterals.Sample.book1!, countLabel: 0),
             SampleEditPickModel(bookImage: ImageLiterals.Sample.book2!, countLabel: 1),
-            SampleEditPickModel(bookImage: ImageLiterals.Sample.book3!, countLabel: nil),
-            SampleEditPickModel(bookImage: ImageLiterals.Sample.book4!, countLabel: nil),
-            SampleEditPickModel(bookImage: ImageLiterals.Sample.book5!, countLabel: nil),
-            SampleEditPickModel(bookImage: ImageLiterals.Sample.book1!, countLabel: nil),
+            SampleEditPickModel(bookImage: ImageLiterals.Sample.book3!, countLabel: 0),
+            SampleEditPickModel(bookImage: ImageLiterals.Sample.book4!, countLabel: 0),
+            SampleEditPickModel(bookImage: ImageLiterals.Sample.book5!, countLabel: 0),
+            SampleEditPickModel(bookImage: ImageLiterals.Sample.book1!, countLabel: 0),
             SampleEditPickModel(bookImage: ImageLiterals.Sample.book2!, countLabel: 3),
-            SampleEditPickModel(bookImage: ImageLiterals.Sample.book3!, countLabel: nil),
-            SampleEditPickModel(bookImage: ImageLiterals.Sample.book4!, countLabel: nil),
-            SampleEditPickModel(bookImage: ImageLiterals.Sample.book5!, countLabel: nil),
-            SampleEditPickModel(bookImage: ImageLiterals.Sample.book1!, countLabel: nil),
-            SampleEditPickModel(bookImage: ImageLiterals.Sample.book2!, countLabel: nil),
-            SampleEditPickModel(bookImage: ImageLiterals.Sample.book3!, countLabel: nil),
+            SampleEditPickModel(bookImage: ImageLiterals.Sample.book3!, countLabel: 0),
+            SampleEditPickModel(bookImage: ImageLiterals.Sample.book4!, countLabel: 0),
+            SampleEditPickModel(bookImage: ImageLiterals.Sample.book5!, countLabel: 0),
+            SampleEditPickModel(bookImage: ImageLiterals.Sample.book1!, countLabel: 0),
+            SampleEditPickModel(bookImage: ImageLiterals.Sample.book2!, countLabel: 0),
+            SampleEditPickModel(bookImage: ImageLiterals.Sample.book3!, countLabel: 0),
             SampleEditPickModel(bookImage: ImageLiterals.Sample.book4!, countLabel: 2),
-            SampleEditPickModel(bookImage: ImageLiterals.Sample.book5!, countLabel: nil),
-            SampleEditPickModel(bookImage: ImageLiterals.Sample.book1!, countLabel: nil),
-            SampleEditPickModel(bookImage: ImageLiterals.Sample.book2!, countLabel: nil),
-            SampleEditPickModel(bookImage: ImageLiterals.Sample.book3!, countLabel: nil),
-            SampleEditPickModel(bookImage: ImageLiterals.Sample.book4!, countLabel: nil),
-            SampleEditPickModel(bookImage: ImageLiterals.Sample.book5!, countLabel: nil),
-            SampleEditPickModel(bookImage: ImageLiterals.Sample.book1!, countLabel: nil),
-            SampleEditPickModel(bookImage: ImageLiterals.Sample.book2!, countLabel: nil),
-            SampleEditPickModel(bookImage: ImageLiterals.Sample.book3!, countLabel: nil),
-            SampleEditPickModel(bookImage: ImageLiterals.Sample.book4!, countLabel: nil),
-            SampleEditPickModel(bookImage: ImageLiterals.Sample.book5!, countLabel: nil)
+            SampleEditPickModel(bookImage: ImageLiterals.Sample.book5!, countLabel: 0),
+            SampleEditPickModel(bookImage: ImageLiterals.Sample.book1!, countLabel: 0),
+            SampleEditPickModel(bookImage: ImageLiterals.Sample.book2!, countLabel: 0),
+            SampleEditPickModel(bookImage: ImageLiterals.Sample.book3!, countLabel: 0),
+            SampleEditPickModel(bookImage: ImageLiterals.Sample.book4!, countLabel: 0),
+            SampleEditPickModel(bookImage: ImageLiterals.Sample.book5!, countLabel: 0),
+            SampleEditPickModel(bookImage: ImageLiterals.Sample.book1!, countLabel: 0),
+            SampleEditPickModel(bookImage: ImageLiterals.Sample.book2!, countLabel: 0),
+            SampleEditPickModel(bookImage: ImageLiterals.Sample.book3!, countLabel: 0),
+            SampleEditPickModel(bookImage: ImageLiterals.Sample.book4!, countLabel: 0),
+            SampleEditPickModel(bookImage: ImageLiterals.Sample.book5!, countLabel: 0)
         ]
     }
 }
