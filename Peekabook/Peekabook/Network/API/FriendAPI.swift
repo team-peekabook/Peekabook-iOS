9864c946e88b6d41ef96a6b9d0c20517c84cad73
6uohul <kiy010604@naver.com>
2023-01-10 17:32:03 +0900

    [Add] #71- FriendAPI에 사용자 검색 추가

--- Peekabook/Peekabook/Network/API/FriendAPI.swift
+++ Peekabook/Peekabook/Network/API/FriendAPI.swift
@@ -16,4 +16,41 @@ final class FriendAPI {
     
     private init() { }
     
+    // 1. 내 책장 (메인 뷰) 조회 하기
+    
+    func searchUserNickname(nickname: String, completion: @escaping (NetworkResult<Any>) -> Void) {
+        friendProvider.request(.getuser(nickname: nickname)) { (result) in
+            switch result {
+            case .success(let response):
+                let statusCode = response.statusCode
+                let data = response.data
+                
+                let networkResult = self.judgeUserSearchStatus(by: statusCode, data)
+                completion(networkResult)
+                
+            case .failure(let err):
+                print(err)
+            }
+        }
+    }
+    
+    private func judgeUserSearchStatus(by statusCode: Int, _ data: Data) -> NetworkResult<Any> {
+        
+        let decoder = JSONDecoder()
+        guard let decodedData = try? decoder.decode(GeneralResponse<SearchUserResponse>.self, from: data)
+        else {
+            return .pathErr
+        }
+        
+        switch statusCode {
+        case 200:
+            return .success(decodedData.data ?? "None-Data")
+        case 400..<500:
+            return .requestErr(decodedData.message as Any)
+        case 500:
+            return .serverErr
+        default:
+            return .networkFail
+        }
+    }
 }
