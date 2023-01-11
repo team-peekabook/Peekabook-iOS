bf1508222dcc36ce61c7d03076bb11ecbf257f33
6uohul <kiy010604@naver.com>
2023-01-11 19:05:57 +0900

    [Feat] #82- 책 추천보기 Response 추가

---
+++ Peekabook/Peekabook/Network/DTO/Recommend/GetRecommendResponse.swift
@@ -0,0 +1,32 @@
+//
+//  GetRecommendResponse.swift
+//  Peekabook
+//
+//  Created by 김인영 on 2023/01/11.
+//
+
+// MARK: - GetRecommendResponse
+struct GetRecommendResponse: Codable {
+    let recommendedBook, recommendingBook: [RecommendBook]
+}
+
+// MARK: - RecommendBook
+struct RecommendBook: Codable {
+    let recommendID: Int
+    let recommendDesc, createdAt: String
+    let friendID: Int
+    let friendNickname: String
+    let friendImage: String
+    let bookID: Int
+    let bookTitle, author: String
+    let bookImage: String
+
+    enum CodingKeys: String, CodingKey {
+        case recommendID = "recommendId"
+        case recommendDesc, createdAt
+        case friendID = "friendId"
+        case friendNickname, friendImage
+        case bookID = "bookId"
+        case bookTitle, author, bookImage
+    }
+}
