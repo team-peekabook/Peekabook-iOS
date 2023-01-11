e4e5772ae753e7656bda56814a41b9a96c73554a
6uohul <kiy010604@naver.com>
2023-01-11 16:11:42 +0900

    [Chore] #79-친구 팔로우 Response 수정

--- Peekabook/Peekabook/Network/DTO/Friend/PostFollowingResponse.swift
+++ Peekabook/Peekabook/Network/DTO/Friend/PostFollowingResponse.swift
@@ -5,16 +5,12 @@
 //  Created by 김인영 on 2023/01/11.
 //
 
+
 import Foundation
 
 // MARK: - PostFollowingResponse
-
 struct PostFollowingResponse: Codable {
-    let followID, receiverID, senderID: Int
-
-    enum CodingKeys: String, CodingKey {
-        case followID = "followId"
-        case receiverID = "receiverId"
-        case senderID = "senderId"
-    }
+    let status: Int
+    let success: Bool
+    let message: String
 }
