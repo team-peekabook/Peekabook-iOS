ba07eb074a98b92066a9844b88ca3c03d0a3d684
dudu <329_143@naver.com>
2023-01-11 19:09:34 +0900

    [Chore] #80 - 컨플릭트 해결

--- Peekabook/Peekabook/Presentation/BookShelf/VC/BookShelfVC.swift
+++ Peekabook/Peekabook/Presentation/BookShelf/VC/BookShelfVC.swift
@@ -19,11 +19,7 @@ final class BookShelfVC: UIViewController {
     private var serverMyBookShelfInfo: MyBookShelfResponse?
     private var friends: [MyIntro] = []
     private var picks: [Pick] = []
-<<<<<<< HEAD
 //    var bookIdPath = 0
-=======
-    var bookIdPath = 0
->>>>>>> #80-책상세보기API연결
     
     // MARK: - UI Components
     
