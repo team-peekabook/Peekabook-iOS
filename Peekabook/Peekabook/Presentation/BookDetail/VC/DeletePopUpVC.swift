d30e42a8072d20f31bbc5e85ac65c0c4718e6066
6uohul <kiy010604@naver.com>
2023-01-12 16:15:08 +0900

    [Feat] #98- 삭제 버튼 눌렀을 때 메인뷰로 화면 전환

--- Peekabook/Peekabook/Presentation/BookDetail/VC/DeletePopUpVC.swift
+++ Peekabook/Peekabook/Presentation/BookDetail/VC/DeletePopUpVC.swift
@@ -113,8 +113,29 @@ extension DeletePopUpVC {
     private func deleteBookAPI(bookId: Int) {
         BookShelfAPI.shared.deleteBook(bookId: bookId) { response in
             if response?.success == true {
-                print("삭제완료")
+                self.switchRootViewController(rootViewController: TabBarController(), animated: true, completion: nil)
             }
         }
     }
 }
+
+extension DeletePopUpVC {
+    func switchRootViewController(rootViewController: UIViewController, animated: Bool, completion: (() -> Void)?) {
+        guard let window = UIApplication.shared.keyWindow else { return }
+        if animated {
+            UIView.transition(with: window, duration: 0.5, options: .transitionCrossDissolve, animations: {
+                let oldState: Bool = UIView.areAnimationsEnabled
+                UIView.setAnimationsEnabled(false)
+                window.rootViewController = rootViewController
+                UIView.setAnimationsEnabled(oldState)
+            }, completion: { (finished: Bool) -> () in
+                if (completion != nil) {
+                    completion!()
+                }
+            })
+        } else {
+            window.rootViewController = rootViewController
+        }
+    }
+}
+
