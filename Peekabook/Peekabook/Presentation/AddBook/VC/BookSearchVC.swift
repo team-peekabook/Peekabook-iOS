3f15b1aac36c3dc55649f0ee0e5cb48dd4a7666d
dudu <329_143@naver.com>
2023-01-14 00:58:06 +0900

    [Fix] #136 - 검색 테이블뷰 안보이는 에러 수정

--- Peekabook/Peekabook/Presentation/AddBook/VC/BookSearchVC.swift
+++ Peekabook/Peekabook/Presentation/AddBook/VC/BookSearchVC.swift
@@ -184,6 +184,7 @@ extension BookSearchVC {
             make.leading.equalTo(view.safeAreaLayoutGuide).offset(20)
             make.trailing.equalTo(view.safeAreaLayoutGuide).inset(20)
             make.bottom.equalToSuperview()
+            make.height.equalTo(128 * bookInfoList.count)
         }
         view.layoutIfNeeded()
     }
@@ -193,7 +194,7 @@ extension BookSearchVC {
             make.top.equalToSuperview()
             make.leading.equalTo(view.safeAreaLayoutGuide).offset(20)
             make.trailing.equalTo(view.safeAreaLayoutGuide).inset(20)
-            make.bottom.equalToSuperview().offset(-1)
+            make.bottom.equalToSuperview()
             make.height.equalTo(128 * bookInfoList.count)
         }
     }
