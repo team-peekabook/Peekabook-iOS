239c7080d281986db3e4cd2e89a0453c54b28cea
6uohul <kiy010604@naver.com>
2023-03-05 18:38:18 +0900

    [Chore] #163- 커스텀 팝업뷰 생성

---
+++ Peekabook/Peekabook/Presentation/UIComponents/CustomPopUpView.swift
@@ -0,0 +1,73 @@
+//
+//  CustomPopUpView.swift
+//  Peekabook
+//
+//  Created by 김인영 on 2023/03/05.
+//
+
+import UIKit
+
+class CustomPopUpView: UIView {
+    
+    let confirmLabel = UILabel().then {
+        $0.text = I18N.BookDelete.popUpComment
+        $0.font = .h4
+        $0.textColor = .peekaRed
+        $0.numberOfLines = 2
+        $0.textAlignment = .center
+    }
+    
+    lazy var cancelButton = UIButton().then {
+        $0.setTitle(I18N.Confirm.cancel, for: .normal)
+        $0.titleLabel!.font = .h2
+        $0.setTitleColor(.white, for: .normal)
+        $0.backgroundColor = .peekaGray2
+    }
+    
+    lazy var confirmButton = UIButton().then {
+        $0.titleLabel!.font = .h1
+        $0.setTitleColor(.white, for: .normal)
+        $0.backgroundColor = .peekaRed
+    }
+    
+    // MARK: - Initialization
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUI()
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension CustomPopUpView {
+    
+    private func setUI() {
+        backgroundColor = .peekaBeige
+    }
+    
+    private func setLayout() {
+        addSubviews(confirmLabel, cancelButton, confirmButton)
+        
+        confirmLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(18)
+            make.centerX.equalToSuperview()
+        }
+        
+        cancelButton.snp.makeConstraints { make in
+            make.top.equalTo(confirmLabel.snp.bottom).offset(14)
+            make.leading.equalToSuperview().offset(16)
+            make.width.equalTo(124)
+            make.height.equalTo(40)
+        }
+        
+        confirmButton.snp.makeConstraints { make in
+            make.top.equalTo(confirmLabel.snp.bottom).offset(14)
+            make.trailing.equalToSuperview().offset(-16)
+            make.width.height.equalTo(cancelButton)
+        }
+    }
+}
