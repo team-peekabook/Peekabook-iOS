47b874141dbe68510b49eaee1907687de4c153b3
dudu <329_143@naver.com>
2023-03-12 12:12:58 +0900

    [Chore] #174 - 네비바 책 추천뷰에 적용

--- Peekabook/Peekabook/Presentation/Proposal/VC/ProposalVC.swift
+++ Peekabook/Peekabook/Presentation/Proposal/VC/ProposalVC.swift
@@ -24,25 +24,15 @@ final class ProposalVC: UIViewController {
 
     // MARK: - UI Components
     
-    private let headerView = UIView()
-    
-    private lazy var backButton = UIButton().then {
-        $0.addTarget(self, action: #selector(backButtonDidTap), for: .touchUpInside)
-        $0.setImage(ImageLiterals.Icn.back, for: .normal)
-    }
-    
-    private let headerTitle = UILabel().then {
-        $0.text = I18N.BookProposal.title
-        $0.font = .h3
-        $0.textColor = .peekaRed
-    }
-    
-    private lazy var checkButton = UIButton().then {
-        $0.setTitle(I18N.BookEdit.done, for: .normal)
-        $0.titleLabel!.font = .h4
-        $0.setTitleColor(.peekaRed, for: .normal)
-        $0.addTarget(self, action: #selector(checkButtonDidTap), for: .touchUpInside)
-    }
+    private lazy var naviBar = CustomNavigationBar(self, type: .oneLeftButtonWithOneRightButton)
+        .addMiddleLabel(title: I18N.BookProposal.title)
+        .addRightButton(with: I18N.BookProposal.done)
+        .addRightButtonAction {
+            self.checkButtonDidTap()
+        }
+        .addLefttButtonAction {
+            self.backButtonDidTap()
+        }
     
     private let containerView = UIScrollView().then {
         $0.showsVerticalScrollIndicator = false
@@ -125,7 +115,6 @@ extension ProposalVC {
     private func setUI() {
         self.view.backgroundColor = .peekaBeige
         
-        headerView.backgroundColor = .clear
         containerView.backgroundColor = .clear
         recommendBoxView.backgroundColor = .white
         recommendHeaderView.backgroundColor = .peekaRed
@@ -135,12 +124,8 @@ extension ProposalVC {
     }
     
     private func setLayout() {
-        [containerView, headerView].forEach {
+        [containerView, naviBar].forEach {
             view.addSubview($0)
-        }
-        
-        [backButton, headerTitle, checkButton].forEach {
-            headerView.addSubview($0)
         }
         
         [bookImgView, nameLabel, authorLabel, recommendBoxView, recommendMaxLabel].forEach {
@@ -156,28 +141,12 @@ extension ProposalVC {
         }
         
         containerView.snp.makeConstraints {
-            $0.top.equalTo(headerView.snp.bottom)
+            $0.top.equalTo(naviBar.snp.bottom)
             $0.leading.trailing.bottom.equalTo(view.safeAreaLayoutGuide)
         }
         
-        headerView.snp.makeConstraints {
+        naviBar.snp.makeConstraints {
             $0.top.leading.trailing.equalTo(view.safeAreaLayoutGuide)
-            $0.height.equalTo(52)
-        }
-        
-        backButton.snp.makeConstraints {
-            $0.centerY.equalToSuperview()
-            $0.leading.equalToSuperview()
-        }
-        
-        headerTitle.snp.makeConstraints {
-            $0.center.equalToSuperview()
-        }
-        
-        checkButton.snp.makeConstraints {
-            $0.centerY.equalToSuperview()
-            $0.trailing.equalToSuperview().inset(11)
-            $0.width.height.equalTo(48)
         }
         
         bookImgView.snp.makeConstraints {
