479803e5ab77f939a89ca165de201df533ce4279
6uohul <kiy010604@naver.com>
2024-02-25 23:14:51 +0900

    [feat]#281- 푸시알림 코드 작성

--- Peekabook/Peekabook/Application/AppDelegate.swift
+++ Peekabook/Peekabook/Application/AppDelegate.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import FirebaseCore
+import FirebaseMessaging
 import KakaoSDKCommon
 
 @main
@@ -32,6 +34,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         KakaoSDK.initSDK(appKey: Config.kakaoNativeAppKey)
         
+        FirebaseApp.configure()
+        setupFCM(application)
+        
         //        if #available(iOS 15, *) {
         //            let appearance = UITabBarAppearance()
         //            appearance.configureWithOpaqueBackground()
@@ -51,4 +56,44 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) { }
     
+    private func setupFCM(_ application: UIApplication) {
+        Messaging.messaging().delegate = self
+        UNUserNotificationCenter.current().delegate = self
+        UNUserNotificationCenter.current().requestAuthorization(options: [.sound, .alert, .badge]) { isAgree, error in
+            if isAgree {
+                print("알림허용")
+            }
+        }
+        application.registerForRemoteNotifications()
+    }
+}
+
+extension AppDelegate: UNUserNotificationCenterDelegate, MessagingDelegate {
+    /// 푸시클릭시
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse) async {
+        print("🟢", #function)
+    }
+    
+    /// 앱화면 보고있는중에 푸시올 때
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification) async -> UNNotificationPresentationOptions {
+        print("🟢", #function)
+        return [.sound, .banner, .list]
+    }
+    
+    /// FCMToken 업데이트시
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        print("🟢", #function, fcmToken)
+    }
+    
+    /// 스위즐링 NO시, APNs등록, 토큰값가져옴
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        Messaging.messaging().apnsToken = deviceToken
+        let deviceTokenString = deviceToken.reduce("", {$0 + String(format: "%02X", $1)})
+        print("🟢", #function, deviceTokenString)
+    }
+    
+    /// error발생시
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        print("🟢", error)
+    }
 }
